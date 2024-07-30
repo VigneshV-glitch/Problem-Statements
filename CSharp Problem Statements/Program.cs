e9f45ba6d1d018f9b38153538618a1f9118894d4
VIGNESH V <vigneshv7678@gmail.com>
2024-07-30 16:20:23 +0530

    Add project files.

---
+++ CSharp Problem Statements/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace CSharp_Problem_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Find the Nearest value of constant integer Of Given Input's
+            //Console.WriteLine("Enter first integer");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter second integer");
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+            //int n = 20;
+
+            //var val1 = Math.Abs(x - n);
+            //var val2 = Math.Abs(y - n);
+
+            //Console.WriteLine(val1 == val2 ? 0 : (val1 < val2 ? x : y));
+            //Console.ReadKey();
+
+
+
+            //swap two numbers without third variable
+            //int num1 = 0, num2 = 0;
+
+            //Console.WriteLine("Enter the number-1");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the number-2");
+            //num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Before Swapping \ta={num1} \tb={num2}", num1, num2);
+
+            //num1 = num1 + num2;
+            //num2 = num1 - num2;
+            //num1 = num1 - num2;
+
+            //Console.WriteLine($"After Swapping \ta={num1} \tb={num2}", num1, num2);
+
+
+
+
+            // Method to calculate the sum of two integers; if they are equal, the sum is tripled
+            //int x = 0, y = 0;
+            //Console.WriteLine("Enter First Integer");
+            //x = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Second Integer");
+            //y = int.Parse(Console.ReadLine());
+            //Console.WriteLine(Inline(x, y));
+
+
+
+
+            //int num1 = 0;
+            //int num2 = 0;
+
+            //Console.WriteLine("Type a number, and then press Enter");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Type a number, and then press enter");
+            //num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Choose an Option from the following list:");
+            //Console.WriteLine("\ta - Add");
+            //Console.WriteLine("\tb - Subtract");
+            //Console.WriteLine("\tc - Multiply");
+            //Console.WriteLine("\td - Divide");
+            //Console.WriteLine("Your Option?");
+
+            //switch(Console.ReadLine())
+            //{
+            //    case "a":
+            //        Console.WriteLine($"Your Result; {num1} + {num2} =" + (num1 + num2));
+            //        break;
+            //    case "b":
+            //        Console.WriteLine($"Your Result; {num1} - {num2} =" + (num1 - num2));
+            //        break;
+            //    case "c":
+            //        Console.WriteLine($"Your Result; {num1} * {num2} =" + (num1 * num2));
+            //        break;
+            //    case "d":
+            //        Console.WriteLine($"Your Result; {num1} / {num2} =" + (num1 / num2));
+            //        break;
+            //}
+            //Console.ReadKey();
+
+
+
+
+            //Console.WriteLine("Odd numbers from 1 to 99. Prints one number per line");
+            //for (int i = 1; i < (99 + 1); i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+        }
+        //public static int Inline(int x, int y)
+        //{
+        //    return x == y ? (x + y) * 3 : x + y;
+        //}
+    }
+}
